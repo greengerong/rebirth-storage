017457d378f5f74671660b326568c565d5807c0b
polang <grzx2210@163.com>
2017-11-06 16:36:32 +0800

    remove settimeout

--- src/rebirth-storage.module.ts
+++ src/rebirth-storage.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { REBIRTH_STORAGE_PROVIDERS } from './rebirth-storage';
 
 @NgModule({
